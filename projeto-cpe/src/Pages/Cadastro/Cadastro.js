352e4d875ac2690cee94f54810ed46024c9410d0
Pedro Rosa <pedrohenri56@yahoo.com>
2021-05-01 12:23:14 -0300

    Página de Cadastro encaminhada

--- projeto-cpe/src/Pages/Cadastro/Cadastro.js
+++ projeto-cpe/src/Pages/Cadastro/Cadastro.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Cadastro.css";
 
 function Cadastro() {
-    return <h1>Cadastro</h1>;
+    
+<header>
+    <button>home</button>
+    
+</header>
+    return (
+
+    <div className= "base">
+        <div className= "centro">       
+            <div className= "Container">
+                <form> 
+                    <h1>Cadastro</h1>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <button>Cadastrar</button>
+        
+                
+                </form>
+            </div>
+        </div> 
+    </div>
+        )
+    
 }
 
 export default Cadastro;
