b5b5f4b8b5cf5b5c27bf7f3f8e2d67de24e342c8
Breno <breno.fluchi@gmail.com>
2021-05-03 09:19:03 -0300

    Criação da página de login

--- projeto-cpe/src/Pages/Home/Home.js
+++ projeto-cpe/src/Pages/Home/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Home.css";
+
 
 function Home() {
-    return <h1>Home</h1>;
+    return <div>
+        <header>
+            <h2>Logo/Nome</h2>
+            <h2>Cadastre-se</h2>
+            <h2>Login</h2>
+        </header>
+        <body>
+            <h1>Home</h1>
+            <h1>História</h1>
+            <h1>Intercâmbios</h1>
+        </body>
+        <footer>
+            <h3>Redes Sociais</h3>
+            <h3>Privacidade e Termos</h3>
+            <h3>Fale Conosco</h3>
+        </footer>
+    </div>;
+
 }
 
 export default Home;
