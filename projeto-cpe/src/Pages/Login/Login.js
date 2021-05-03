1a6b04676abebae3d82cbfdfbd169d8efdcae7e4
Breno <breno.fluchi@gmail.com>
2021-05-03 11:32:33 -0300

    Login quase pronto
    Falta apenas o cabeçalho e o rodapé

--- projeto-cpe/src/Pages/Login/Login.js
+++ projeto-cpe/src/Pages/Login/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./Login.css";
 
-function Login() {
-    return <h1>Login</h1>;
+function Login() 
+{
+    return (
+    <div className = "base">
+        <div className = "container">
+            <div >
+                <div className = "imagem">
+                    <img src = "/images/logoreduzida.png" alt = "Volare Intercâmbios"/>
+                </div>
+                <Form className = "inputs">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Email" />
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="password" placeholder="Senha" />
+                </Form.Group>
+                <Button variant="warning" >Login</Button>{''}  
+                </Form>
+            </div>
+        </div>    
+    </div>
+    )
 }
 
 export default Login;
