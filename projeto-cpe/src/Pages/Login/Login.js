d6ffd82beb3472f6ae11643ff4257312809536f7
Breno <breno.fluchi@gmail.com>
2021-05-04 11:40:10 -0300

    Login funcional e com a logo nova
    Necessária alterações no css

--- projeto-cpe/src/Pages/Login/Login.js
+++ projeto-cpe/src/Pages/Login/Login.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./Login.css";
+import { useHistory } from 'react-router-dom';
 
 function Login() 
 {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const history = useHistory();
+    function login()
+    {
+        alert("Bem vindo!\n" + email)
+        history.push("home")
+    }
     return (
     <div className = "base">
         <div className = "container">
             <div >
                 <div className = "imagem">
-                    <img src = "/images/logoreduzida.png" alt = "Volare Intercâmbios"/>
+                    <img src = "/images/logo4reduzida.png" alt = "Volare Intercâmbios"/>
                 </div>
                 <Form className = "inputs">
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Control type="email" placeholder="Email" onChange = {(e) => setEmail(e.target.value)}/>
                 </Form.Group>
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Control type="password" placeholder="Senha" />
+                    <Form.Control type="password" placeholder="Senha" onChange = {(e) => setPassword(e.target.value)} />
                 </Form.Group>
-                <Button variant="warning" >Login</Button>{''}  
+                <Button variant="warning" style = {{backgroundColor: "rgb(255, 183, 3)"}} onClick={login}>Login</Button>{''}  
                 </Form>
             </div>
         </div>    
