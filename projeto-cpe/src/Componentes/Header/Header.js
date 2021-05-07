a79b9d8e2f887c2ac0ee76ab0c149a7781017298
Pedro Rosa <pedrohenri56@yahoo.com>
2021-05-07 17:51:41 -0300

    Tentativa de Header geral 1

--- projeto-cpe/src/Componentes/Header/Header.js
+++ projeto-cpe/src/Componentes/Header/Header.js
@@ -1,18 +1,28 @@
 import React from "react";
 import "./Header.css";
+import {Button} from "react-bootstrap";
 
 function Header() {
     return (
     
     <div className= "baseHeader">
-        <div className= ""></div>
-            <h1>Header</h1>
-
+        <div className= "Header"></div>
+        
+            <div className="HeaderLogo">
+               <a href="./home" ><img src = "/images/logo4menorainda.png" alt = "Volare Intercâmbios"/></a>
+            </div>
+            <div className="Header-botoes">
+            <div className="HeaderLogin">
+                <p><a href="/login" title="login" target="_blank" rel="noreferrer"><Button variant="warning">Login</Button></a></p>
+            </div>
+           
+            <div className="HeaderCadastrar">
+                <p><a href="/cadastro" title="cadastro" target="_blank" rel="noreferrer"><Button variant="warning">Cadastrar</Button></a></p>
+            </div>
+            </div>
+            
+            
     </div>
-    
-    
-    
-    
     )
 }
 export default Header;
