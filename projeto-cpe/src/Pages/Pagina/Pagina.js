5752a52ea9d60bda46b1008c0bf0f708865d547d
Bruna Dias <brunandradias@gmail.com>
2021-05-10 16:19:05 -0300

    Página de intercâmbios quase pronta

--- projeto-cpe/src/Pages/Pagina/Pagina.js
+++ projeto-cpe/src/Pages/Pagina/Pagina.js
@@ -1,7 +1,37 @@
 import React from "react";
+import "./Pagina.css";
 
-function Pagina() {
-    return <h1>Pagina</h1>;
+
+function Intercambios() {
+    return (
+        <div className = "Intercambios">
+
+            <div className = "titulo"> 
+
+                <h1>Nossos destinos</h1>
+
+            </div>  
+
+            <div className = "Brighton">
+            
+            
+                <div className = "caixab">
+                    <img src = "./imagespag/brighton.png" alt = "Brighton"/>
+
+                    
+                        <p><h1>Inglês em Brighton</h1>
+                        <h4>3 semanas de curso de Inglês na escola Ardmore, com acomodação e refeições inclusas e passeios para Brighton Pier, Londres, Harry Potter Studios e muito mais!</h4>
+                        <h1>12x de R$ 1.000,00</h1></p>
+
+                
+            </div>
+                   
+
+        </div>
+        </div>
+
+    )   
+    
 }
 
-export default Pagina;
+export default Intercambios;
