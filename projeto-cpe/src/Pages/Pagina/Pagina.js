7b472dfd39618f3e3049c6625d8aca003190a3d5
Bruna Dias <brunandradias@gmail.com>
2021-05-12 19:08:38 -0300

    Código destino esquerda e direita pronto

--- projeto-cpe/src/Pages/Pagina/Pagina.js
+++ projeto-cpe/src/Pages/Pagina/Pagina.js
@@ -32,12 +32,12 @@ function Intercambios() {
             
             
                 <div className = "caixama">
-                    <img src = "./imagespag/madrid.png" alt = "Madrid"/>
-
-                    
+                                        
                         <p><h1>Inglês em Brighton</h1>
                         <h4>3 semanas de curso de Inglês na escola Ardmore, com acomodação e refeições inclusas e passeios para Brighton Pier, Londres, Harry Potter Studios e muito mais!</h4>
                         <h1>12x de R$ 1.000,00</h1></p>
+
+                        <img src = "./imagespag/madrid.png" alt = "Madrid"/>
 
                 
                 </div>
