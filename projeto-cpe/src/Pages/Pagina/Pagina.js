7be5dcbabfff9df331d3968a51e9d55f887ad325
Bruna Dias <brunandradias@gmail.com>
2021-05-12 18:47:06 -0300

    Teste dos demais destinos

--- projeto-cpe/src/Pages/Pagina/Pagina.js
+++ projeto-cpe/src/Pages/Pagina/Pagina.js
@@ -24,10 +24,27 @@ function Intercambios() {
                         <h1>12x de R$ 1.000,00</h1></p>
 
                 
-            </div>
-                   
+                </div>
 
-        </div>
+            </div>    
+
+            <div className = "Madrid">
+            
+            
+                <div className = "caixama">
+                    <img src = "./imagespag/madrid.png" alt = "Madrid"/>
+
+                    
+                        <p><h1>Inglês em Brighton</h1>
+                        <h4>3 semanas de curso de Inglês na escola Ardmore, com acomodação e refeições inclusas e passeios para Brighton Pier, Londres, Harry Potter Studios e muito mais!</h4>
+                        <h1>12x de R$ 1.000,00</h1></p>
+
+                
+                </div>
+                    
+
+            </div>
+
         </div>
 
     )   
