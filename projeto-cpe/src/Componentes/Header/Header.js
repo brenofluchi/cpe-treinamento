2ae13cd0e6223c45f5143a5b16bc6f34d4c9cbdf
Breno Luchi <breno.fluchi@gmail.com>
2021-05-14 15:37:51 -0300

    Header pronto responsivo
    Falta só decidirmos como vamos fazer com as outras abas!

--- projeto-cpe/src/Componentes/Header/Header.js
+++ projeto-cpe/src/Componentes/Header/Header.js
@@ -1,35 +1,39 @@
 import React from "react";
 import "./Header.css";
-import {Button} from "react-bootstrap";
+
 
 function Header() {
     return (
     
     <div className= "baseHeader">
-        <div className="HeaderLogo">
-            <a href="./home" ><img src = "/images/logonovo.png"  title="Home" alt = "Volare Intercâmbios"/></a>
+        <div className="header">
+            <div className="headerLogo">
+                <a href="./home" ><img src = "/images/logonovoheader.png"  title="Home" alt = "Volare Intercâmbios"/></a>
+            </div>
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <a href ="/home" title="Home">HOME</a>
+                    </li>
+                    <li>
+                        <a href ="/pagina" title="Destinos">DESTINOS</a>
+                    </li>
+                    <li>
+                        <a href ="/historia" title="Quem somos">QUEM SOMOS</a>
+                    </li>     
+                </ul>
+            </nav>
+            <nav id="usuario">
+                <ul>
+                    <li>
+                        <a href="/cadastro" title="Cadastro" target="_blank" rel="noreferrer">Cadastre-se</a>
+                    </li>
+                    <li>
+                        <a href="login" title="Login" target="_blank" rel="noreferrer">Login</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        <nav id="menu">
-            <ul>
-                <li>
-                    <a href ="/home" title="Home">HOME</a>
-                </li>
-                <li>
-                    <a href ="/pagina" title="Destinos">DESTINOS</a>
-                </li>
-                <li>
-                    <a href ="/historia" title="Quem somos">QUEM SOMOS</a>
-                </li>     
-            </ul>
-        </nav>
-        <div className="Header-botoes">
-            <div className="HeaderLogin">
-                <p><a href="/login" title="Login" target="_blank" rel="noreferrer">Login</a></p>
-            </div>
-            <div className="HeaderCadastrar">
-                <p><a href="/cadastro" title="Cadastro" target="_blank" rel="noreferrer">Cadastrar</a></p>
-            </div>
-        </div>    
     </div>
     )
 }
